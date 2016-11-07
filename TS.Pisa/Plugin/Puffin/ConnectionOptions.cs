5cc02cf7047672bd3ac5913d1782f9c58e4535dd
Paul MacDonald <paul.macdonald@tradingscreen.com>
2016-11-07 11:21:38 +0000

    Change to getter and setter methods so will work with C#4

--- TS.Pisa/Plugin/Puffin/ConnectionOptions.cs
+++ TS.Pisa/Plugin/Puffin/ConnectionOptions.cs
@@ -7,29 +7,66 @@ namespace TS.Pisa.Plugin.Puffin
     /// </summary>
     public class ConnectionOptions : ICloneable
     {
+        private bool _ZipRequests = true;
+        private bool _ZipPrices = true;
+        private bool _Encrypt = true;
+        private bool _SSL = false;
+
         /// <summary>
         /// Determines if subscription requests are compressed.
-        /// True if configured to compress subscriptions.
         /// </summary>
-        public bool ZipRequests { get; set; } = true;
-        
+        /// <returns>True if configured to compress subscriptions.</returns>
+        public bool IsZipRequests()
+        {
+            return _ZipRequests;
+        }
+
+        public void SetZipRequests(bool ZipRequests)
+        {
+            _ZipRequests = ZipRequests;
+        }
+
         /// <summary>
-        /// Determines if price updates are compressed by the server.
-        /// True if the server is configured to compress price updates.
+        /// Determines if price updates should be compressed by the server.
         /// </summary>
-        public bool ZipPrices { get; set; } = true;
+        /// <returns>True if price updates are required to be compressed by the server.</returns>
+        public bool IsZipPrices()
+        {
+            return _ZipPrices;
+        }
+
+        public void SetZipPrices(bool ZipPrices)
+        {
+            _ZipPrices = ZipPrices;
+        }
 
         /// <summary>
         /// Get the encrypt I/O flag.
-        /// True if configured to encrypt all I/O communications.
         /// </summary>
-        public bool Encrypt { get; set; } = true;
+        /// <returns>True if configured to encrypt all I/O communications.</returns>
+        public bool IsEncrypt()
+        {
+            return _Encrypt;
+        }
+
+        public void SetEncrypt(bool Encrypt)
+        {
+            _Encrypt = Encrypt;
+        }
 
         /// <summary>
         /// Determine if an SSL session should be established with the server.
-        /// True if configured to establish an SSL connection.
         /// </summary>
-        public bool SSL { get; set; } = false;
+        /// <returns>True if configured to establish SSL session.</returns>
+        public bool IsSSL()
+        {
+            return _SSL;
+        }
+
+        public void SetSSL(bool SSL)
+        {
+            _SSL = SSL;
+        }
 
         public object Clone()
         {
