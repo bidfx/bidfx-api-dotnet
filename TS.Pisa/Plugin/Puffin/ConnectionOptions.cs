0a23d91214c7e3ac8af29a38baba0687f4cd1c81
Paul MacDonald <paul.macdonald@tradingscreen.com>
2016-11-07 09:58:53 +0000

    Reformat ConnectionOptions

--- TS.Pisa/Plugin/Puffin/ConnectionOptions.cs
+++ TS.Pisa/Plugin/Puffin/ConnectionOptions.cs
@@ -12,16 +12,19 @@ namespace TS.Pisa.Plugin.Puffin
         /// True if configured to compress subscriptions.
         /// </summary>
         public bool ZipRequests { get; set; } = true;
+        
         /// <summary>
         /// Determines if price updates are compressed by the server.
         /// True if the server is configured to compress price updates.
         /// </summary>
         public bool ZipPrices { get; set; } = true;
+
         /// <summary>
         /// Get the encrypt I/O flag.
         /// True if configured to encrypt all I/O communications.
         /// </summary>
         public bool Encrypt { get; set; } = true;
+
         /// <summary>
         /// Determine if an SSL session should be established with the server.
         /// True if configured to establish an SSL connection.
