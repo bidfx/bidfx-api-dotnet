369d00bf7c5c6ec85408845ff2c6c90f7e305740
Paul Sweeny <paul.sweeny@tradingscreen.com>
2016-11-14 13:41:17 +0000

    Get all tests to pass

--- TS.Pisa.Test/Plugin/Puffin/Xml/XmlElementTest.cs
+++ TS.Pisa.Test/Plugin/Puffin/Xml/XmlElementTest.cs
@@ -100,8 +100,8 @@ namespace TS.Pisa.Plugin.Puffin.Xml
         {
             XmlElement nested = new XmlElement(FxPrice).Add(Bid, 22);
             XmlElement element = new XmlElement(Update).Add(nested);
-            Assert.AreEqual("<Update><FxPrice Bid=\"22\"/></Update>", element.ToString());
             XmlElement first = element.GetContents().GetFirstElement();
+            Assert.AreEqual("<Update><FxPrice Bid=\"22\"/></Update>", element.ToString());
             Assert.AreEqual("<FxPrice Bid=\"22\"/>", first.ToString());
         }
 
