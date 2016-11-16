e71a9f72acc93e2e8cff25480ff900e57537c251
Paul Sweeny <paul.sweeny@tradingscreen.com>
2016-11-16 11:28:11 +0000

    test availablility of properties

--- TS.Pisa.Example/Program.cs
+++ TS.Pisa.Example/Program.cs
@@ -1,11 +1,13 @@
 ﻿using System;
+using System.Net.NetworkInformation;
+using System.Reflection;
 using TS.Pisa.Plugin.Puffin;
 namespace TS.Pisa.Example
 {
     internal class Program
     {
         private static readonly log4net.ILog log =
-            log4net.LogManager.GetLogger(System.Reflection.MethodBase.GetCurrentMethod().DeclaringType);
+            log4net.LogManager.GetLogger(MethodBase.GetCurrentMethod().DeclaringType);
         public static void Main(string[] args)
         {
             var program = new Program();
@@ -13,6 +15,11 @@ namespace TS.Pisa.Example
         }
         private void Test()
         {
+            var reference = PisaVersion.Reference;
+            log.Info("pisa version:    " + PisaVersion.Version);
+            log.Info("program version: " + Assembly.GetExecutingAssembly().GetName().Version);
+            log.Info("user:            " + System.Security.Principal.WindowsIdentity.GetCurrent().Name);
+            log.Info("host:            " + IPGlobalProperties.GetIPGlobalProperties().HostName);
             var session = PrepareSession();
             session.Subscribe("AssetClass=FixedIncome,Exchange=SGC,Level=1,Source=Lynx,Symbol=DE000A14KK32");
             session.Subscribe("AssetClass=FixedIncome,Exchange=SGC,Level=1,Source=Lynx,Symbol=XS1083955911");
