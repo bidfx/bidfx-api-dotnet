b87e058c549f4db28584402a90f74239f50df578
Paul MacDonald <paul.macdonald@tradingscreen.com>
2016-12-08 10:12:34 +0000

    Change to correct version number

--- SharedAssemblyInfo.cs
+++ SharedAssemblyInfo.cs
@@ -1,6 +1,6 @@
 using System.Reflection;
 
-[assembly: AssemblyVersion("1.2.1")]
-[assembly: AssemblyFileVersion("1.2.1")]
+[assembly: AssemblyVersion("1.0.1")]
+[assembly: AssemblyFileVersion("1.0.1")]
 [assembly: AssemblyCopyright("Copyright ©  2016")]
 [assembly: AssemblyCompany("TradingScreen Inc.")]
