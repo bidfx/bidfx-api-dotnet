1aeae48c431de26614d25aff87c94e6c855e1fdd
Paul MacDonald <paulrm6@gmail.com>
2016-12-11 19:27:18 +0000

    Add the provider status event to the FI session

--- TS.Pisa.FI.Example/FieldAccessTest.cs
+++ TS.Pisa.FI.Example/FieldAccessTest.cs
@@ -11,6 +11,7 @@ namespace TS.Pisa.FI.Example
             _session = session;
             session.OnPrice += OnPrice;
             session.OnStatus += OnStatus;
+            session.OnProviderStatus += OnProviderStatus;
         }
 
         private static void OnPrice(object source, PriceUpdateEventArgs priceEvent)
@@ -25,6 +26,11 @@ namespace TS.Pisa.FI.Example
         public void OnStatus(object source, SubscriptionStatusEventArgs statusEvent)
         {
             Console.WriteLine(statusEvent.Subject.Isin + " " + statusEvent.Status + " - " + statusEvent.Reason);
+        }
+
+        private void OnProviderStatus(object source, ProviderPluginEventArgs providerEvent)
+        {
+            Console.WriteLine(providerEvent);
         }
 
         private void SendSubscriptions()
