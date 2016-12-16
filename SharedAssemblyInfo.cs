69c31f2c9bf411dbea5fc47357426cf524a5ae82
Paul MacDonald <pmacdona@tradingscreen.com>
2016-12-16 16:52:10 +0000

    prepare for next development iteration

--- SharedAssemblyInfo.cs
+++ SharedAssemblyInfo.cs
@@ -1,6 +1,6 @@
 using System.Reflection;
 
-[assembly: AssemblyVersion("1.0.1")]
-[assembly: AssemblyFileVersion("1.0.1")]
+[assembly: AssemblyVersion("1.0.2")]
+[assembly: AssemblyFileVersion("1.0.2")]
 [assembly: AssemblyCopyright("Copyright ©  2016")]
 [assembly: AssemblyCompany("TradingScreen Inc.")]
