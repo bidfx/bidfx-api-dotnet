e136438bc588cd230a5324411f5677853c014be0
Paul MacDonald <Paul.MacDonald@TradingScreen.com>
2017-06-15 11:52:59 +0100

    Add nano time methods

--- BidFX.Public.API/src/Price/Tools/JavaTime.cs
+++ BidFX.Public.API/src/Price/Tools/JavaTime.cs
@@ -11,9 +11,19 @@ namespace BidFX.Public.API.Price.Tools
             return ToJavaTime(DateTime.UtcNow);
         }
 
+        public static long NanoTime()
+        {
+            return ToJavaTimeNano(DateTime.UtcNow);
+        }
+
         public static long ToJavaTime(DateTime dateTime)
         {
             return (long) (dateTime.ToUniversalTime() - Jan1st1970).TotalMilliseconds;
+        }
+
+        public static long ToJavaTimeNano(DateTime dateTime)
+        {
+            return (dateTime.ToUniversalTime() - Jan1st1970).Ticks;
         }
 
         public static DateTime ToDateTime(long javaTime)
