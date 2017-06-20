180edfd0943809c83a33c8c4bc5468b00e67c1ec
Paul MacDonald <paulrm6@gmail.com>
2017-06-20 16:17:06 +0100

    Add AckData.cs

--- BidFX.Public.API/src/Price/Plugin/Pixie/AckData.cs
+++ BidFX.Public.API/src/Price/Plugin/Pixie/AckData.cs
@@ -1,7 +1,36 @@
+using System;
+using BidFX.Public.API.Price.Plugin.Pixie.Messages;
+using BidFX.Public.API.Price.Tools;
+
 namespace BidFX.Public.API.Price.Plugin.Pixie
 {
     public class AckData
     {
+        public long Revision { get; set; }
+        public long RevisionTime { get; set; }
+        public long PriceReceivedTime { get; set; }
+        public long HandlingStartNanoTime { get; set; }
         
+        
+        public AckMessage ToAckMessage()
+        {
+            var ackTime = JavaTime.CurrentTimeMillis();
+            var handlingEndNanoTime = JavaTime.NanoTime();
+            return ToAckMessage(ackTime, handlingEndNanoTime);
+        }
+
+        private AckMessage ToAckMessage(long ackTime, long handlingEndNanoTime)
+        {
+            var handlingDuration = Math.Max(0, handlingEndNanoTime - HandlingStartNanoTime);
+            var handlingDurationMicros = (handlingDuration + 500L) / 1000L;
+            return new AckMessage
+            {
+                AckTime = ackTime,
+                HandlingDuration = handlingDurationMicros,
+                PriceReceivedTime = PriceReceivedTime,
+                Revision = Revision,
+                RevisionTime = RevisionTime
+            };
+        }
     }
 }
