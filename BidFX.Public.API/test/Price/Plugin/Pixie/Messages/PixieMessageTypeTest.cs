8f93f4d1127ff66eb454d6f40f63c1783a8ae1ee
Paul MacDonald <Paul.MacDonald@TradingScreen.com>
2017-06-27 15:49:58 +0100

    Add pixie message type tests

---
+++ BidFX.Public.API/test/Price/Plugin/Pixie/Messages/PixieMessageTypeTest.cs
@@ -0,0 +1,19 @@
+﻿using NUnit.Framework;
+
+namespace BidFX.Public.API.Price.Plugin.Pixie.Messages
+{
+    public class PixieMessageTypeTest
+    {
+        [Test]
+        public void TestConstantValues()
+        {
+            Assert.AreEqual('A', PixieMessageType.Ack);
+            Assert.AreEqual('P', PixieMessageType.PriceSync);
+            Assert.AreEqual('S', PixieMessageType.SubscriptionSync);
+            Assert.AreEqual('D', PixieMessageType.DataDictionary);
+            Assert.AreEqual('G', PixieMessageType.Grant);
+            Assert.AreEqual('H', PixieMessageType.Heartbeat);
+            Assert.AreEqual('W', PixieMessageType.Welcome);
+        }
+    }
+}
