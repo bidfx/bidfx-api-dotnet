f0fa6467b1701bbf010e81d018cf5efc6d86b92c
Paul MacDonald <Paul.MacDonald@TradingScreen.com>
2017-06-28 11:22:30 +0100

    Add welcome message tests

--- BidFX.Public.API/test/Price/Plugin/Pixie/Messages/WelcomeMessageTest.cs
+++ BidFX.Public.API/test/Price/Plugin/Pixie/Messages/WelcomeMessageTest.cs
@@ -1,7 +1,22 @@
-﻿namespace BidFX.Public.API.Price.Plugin.Pixie.Messages
+﻿using BidFX.Public.API.Price.Tools;
+using NUnit.Framework;
+
+namespace BidFX.Public.API.Price.Plugin.Pixie.Messages
 {
     public class WelcomeMessageTest
     {
+        private const string EncodedMessage = "570001000010e1000026ae";
         
+        [Test]
+        public void decodes_a_buffer_into_a_message()
+        {
+            var memoryStream = VarintTest.HexStream(EncodedMessage);
+            Assert.AreEqual(PixieMessageType.Welcome, memoryStream.ReadByte());
+            var message = new WelcomeMessage(memoryStream);
+            Assert.AreEqual(0, message.Options);
+            Assert.AreEqual(1, message.Version);
+            Assert.AreEqual(4321, message.ClientId);
+            Assert.AreEqual(9902, message.ServerId);
+        }
     }
 }
