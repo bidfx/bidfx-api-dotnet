61898ed2d8dea7745eb0657ecf2ecfb6c1dc01ba
Liam Asman <Liam.Asman@TradingScreen.com>
2018-07-05 10:21:29 +0100

    prepare for next development iteration

--- BidFX.Public.API/Properties/AssemblyInfo.cs
+++ BidFX.Public.API/Properties/AssemblyInfo.cs
@@ -1,31 +1,31 @@
-﻿using System.Reflection;
-using System.Runtime.CompilerServices;
-using System.Runtime.InteropServices;
-
-// General Information about an assembly is controlled through the following 
-// set of attributes. Change these attribute values to modify the information
-// associated with an assembly.
-
-[assembly: AssemblyVersion("1.1.5")]
-[assembly: AssemblyFileVersion("1.1.5")]
-[assembly: AssemblyCopyright("Copyright © 2018")]
-[assembly: AssemblyCompany("BidFX Limited")]
-[assembly: AssemblyTitle("BidFX.Public.API")]
-[assembly: AssemblyDescription("")]
-[assembly: AssemblyConfiguration("")]
-[assembly: AssemblyProduct("BidFX.Public.API")]
-[assembly: AssemblyTrademark("")]
-[assembly: AssemblyCulture("")]
-
-// Allow internals visibile to tests
-[assembly: InternalsVisibleTo("DynamicProxyGenAssembly2")]
-
-// Setting ComVisible to false makes the types in this assembly not visible 
-// to COM components. If you need to access a type in this assembly from 
-// COM, set the ComVisible attribute to true on that type.
-
-[assembly: ComVisible(false)]
-
-// The following GUID is for the ID of the typelib if this project is exposed to COM
-
-[assembly: Guid("096EDEB7-9912-45BC-9C51-A7E871E58C4E")]
+[assembly: AssemblyFileVersion("1.1.6")]
+[assembly: AssemblyFileVersion("1.1.6")]
+[assembly: AssemblyFileVersion("1.1.6")]
+[assembly: AssemblyFileVersion("1.1.6")]
+[assembly: AssemblyFileVersion("1.1.6")]
+[assembly: AssemblyFileVersion("1.1.6")]
+[assembly: AssemblyFileVersion("1.1.6")]
+[assembly: AssemblyFileVersion("1.1.6")]
+[assembly: AssemblyVersion("1.1.6")]
+[assembly: AssemblyFileVersion("1.1.6")]
+[assembly: AssemblyFileVersion("1.1.6")]
+[assembly: AssemblyFileVersion("1.1.6")]
+[assembly: AssemblyFileVersion("1.1.6")]
+[assembly: AssemblyFileVersion("1.1.6")]
+[assembly: AssemblyFileVersion("1.1.6")]
+[assembly: AssemblyFileVersion("1.1.6")]
+[assembly: AssemblyFileVersion("1.1.6")]
+[assembly: AssemblyFileVersion("1.1.6")]
+[assembly: AssemblyFileVersion("1.1.6")]
+[assembly: AssemblyFileVersion("1.1.6")]
+[assembly: AssemblyFileVersion("1.1.6")]
+[assembly: AssemblyFileVersion("1.1.6")]
+[assembly: AssemblyFileVersion("1.1.6")]
+[assembly: AssemblyFileVersion("1.1.6")]
+[assembly: AssemblyFileVersion("1.1.6")]
+[assembly: AssemblyFileVersion("1.1.6")]
+[assembly: AssemblyFileVersion("1.1.6")]
+[assembly: AssemblyFileVersion("1.1.6")]
+[assembly: AssemblyFileVersion("1.1.6")]
+[assembly: AssemblyFileVersion("1.1.6")]
+[assembly: AssemblyFileVersion("1.1.6")]
