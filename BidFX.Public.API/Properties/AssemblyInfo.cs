4aebad2c58c0a515fa76e6ba23ce73dcd6b91370
Liam Asman <Liam.Asman@tradingscreen.com>
2019-06-26 12:38:55 +0100

    prepare for next development iteration

--- BidFX.Public.API/Properties/AssemblyInfo.cs
+++ BidFX.Public.API/Properties/AssemblyInfo.cs
@@ -6,8 +6,8 @@ using System.Runtime.InteropServices;
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
 
-[assembly: AssemblyVersion("1.4.2")]
-[assembly: AssemblyFileVersion("1.4.2")]
+[assembly: AssemblyVersion("1.4.3")]
+[assembly: AssemblyFileVersion("1.4.3")]
 [assembly: AssemblyCopyright("Copyright © 2018")]
 [assembly: AssemblyCompany("BidFX Limited")]
 [assembly: AssemblyTitle("BidFX.Public.API")]
