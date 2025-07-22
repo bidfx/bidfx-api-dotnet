6e061ad00816402787284b0f80911b06bf6f0d93
Richard Manthorpe <rmanthorpe@symmetryinvestments.com>
2025-07-22 09:22:14 +0100

    Convert to modern SDK-style project
    This conversion is necessary to target .NET Standard or .NET Core

--- BidFX.Public.API/Properties/AssemblyInfo.cs
+++ BidFX.Public.API/Properties/AssemblyInfo.cs
@@ -1,21 +1,6 @@
-using System.Reflection;
+﻿using System.Reflection;
 using System.Runtime.CompilerServices;
 using System.Runtime.InteropServices;
-
-// General Information about an assembly is controlled through the following 
-// set of attributes. Change these attribute values to modify the information
-// associated with an assembly.
-
-[assembly: AssemblyVersion("1.7.3")]
-[assembly: AssemblyFileVersion("1.7.3")]
-[assembly: AssemblyCopyright("Copyright © 2018")]
-[assembly: AssemblyCompany("BidFX Limited")]
-[assembly: AssemblyTitle("BidFX.Public.API")]
-[assembly: AssemblyDescription("")]
-[assembly: AssemblyConfiguration("")]
-[assembly: AssemblyProduct("BidFX.Public.API")]
-[assembly: AssemblyTrademark("")]
-[assembly: AssemblyCulture("")]
 
 // Allow internals visibile to tests
 [assembly: InternalsVisibleTo("DynamicProxyGenAssembly2")]
